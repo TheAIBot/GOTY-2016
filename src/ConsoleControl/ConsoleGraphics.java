ff4cb50c4a8878fba7b79b1afcd113c12766a902
TheAIBot <andreasuranus@hotmail.com>
2016-01-05 15:13:45 +0100

    changed ConsoleGraphics to use String.format to align the tiles

--- src/ConsoleControl/ConsoleGraphics.java
+++ src/ConsoleControl/ConsoleGraphics.java
@@ -1,42 +1,28 @@
 package ConsoleControl;
 
-import java.awt.Point;
-import java.util.Arrays;
-
-import com.sun.xml.internal.ws.util.StringUtils;
-
 import GameEngine.GameEngine;
 import GameEngine.Tile;
 
 class ConsoleGraphics {
 	public static void printGame(GameEngine game)
 	{
-		int boardSize = game.getBoardSize();
-		int tileLength = (int)Math.log10((boardSize * boardSize) - 1) + 1;
-		int spacesBetweenTiles = 2;
-		Tile[] tiles = game.getTiles();
-		char[] line = new char[tileLength * boardSize + (boardSize - 1) * spacesBetweenTiles];
-		Arrays.fill(line, ' ');
+		final int boardSize = game.getBoardSize();
+		final int spacesBetweenTileNumbers = 2;
+		final int tileLength = (int)Math.log10((boardSize * boardSize) - 1) + 1 + spacesBetweenTileNumbers;
+		final String sTileLength = String.valueOf(tileLength);
+		final Tile[] tiles = game.getTiles();
 		for (int y = 0; y < boardSize; y++) {
-			int numberOfSpaces = 0;
-			for (int x = 0; x < game.getBoardSize(); x++) {
-				Tile tile = tiles[x + y * game.getBoardSize()];
-				char[] number;
+			for (int x = 0; x < boardSize; x++) {
+				Tile tile = tiles[x + y * boardSize];
 				if (tile == null) {
-					 number = new char[tileLength];
-					 Arrays.fill(number, ' ');
+					System.out.print(String.format("%" + sTileLength + "s", ""));
 				}
 				else {
-					number = String.valueOf(tile.number).toCharArray();
+					int tileNumber = tile.number;
+					System.out.print(String.format("%" + sTileLength + "d", tileNumber));
 				}
-				int offset = (tileLength * (x + 1)) + 
-						 	 (spacesBetweenTiles * numberOfSpaces) - 
-						 	  number.length;
-				System.arraycopy(number, 0, line, offset, number.length);
-				numberOfSpaces++;
 			}
-			System.out.println(new String(line));
-			Arrays.fill(line, ' ');
+			System.out.println("");			
 		}
 		System.out.println("");
 		System.out.println("");
