b0802c95c9b3687f56f31c78836c257d0e393ccb
TheAIBot <andreasuranus@hotmail.com>
2016-01-06 00:43:18 +0100

    added argument check to Log methods

--- src/Log/Log.java
+++ src/Log/Log.java
@@ -9,6 +9,9 @@ public class Log {
 	
 	public static void writeError(Exception e)
 	{
+		if (e == null) {
+			throw new IllegalArgumentException("error is null");
+		}
 		writeln(e.getMessage());
 		e.printStackTrace(getLogStream());
 	}
