ed6c3e6f5ffa92f3e769ce20230bb932e471372a
TheAIBot <andreasuranus@hotmail.com>
2016-01-07 09:29:15 +0100

    finished DifficultyCalculator

--- src/Model/DifficultyCalculator.java
+++ src/Model/DifficultyCalculator.java
@@ -1,8 +1,46 @@
 package Model;
 
+import java.awt.Point;
+
 public class DifficultyCalculator {
-	public static void CalculateDifficulty()
+	public static DifficultyLevel getDifficulty(Tile[] tiles, int size)
 	{
-		
+		double difficulty = 0;
+		for (int i = 0; i < tiles.length; i++) {
+			if (tiles[i] != null) {
+				difficulty += CalculateDistance(size, convertIndexToPoint(i, size), tiles[i].position);
+			}
+		}
+		double difficultyPercentage = difficulty / getMaxDifficulty(size);
+		return DifficultyLevel.getDifficultylevelFromPercentDifficult(difficultyPercentage);
+	}
+	
+	private static double CalculateDistance(int size, Point a, Point b)
+	{
+		Point ba = new Point((int)Math.abs(a.getX() - b.getX()),
+							 (int)Math.abs(a.getY() - b.getY()));
+		return ba.getX() + ba.getY();
+	}
+	
+	private static Point convertIndexToPoint(int index, int size)
+	{
+			int row = index / size;
+			int col = index % size;
+
+			return new Point(col, row);
+	}
+	
+	private static double getMaxDifficulty(int size)
+	{
+		final int maxNumber = size * size;
+		int expectedNumber = maxNumber;
+		double difficulty = 0;
+		for (int y = 0; y < size; y++) {
+			for (int x = 0; x < size; x++) {
+				difficulty += CalculateDistance(size, convertIndexToPoint(maxNumber - expectedNumber, size), convertIndexToPoint(expectedNumber, size));
+				expectedNumber++;
+			}
+		}
+		return (double)difficulty;
 	}
 }
