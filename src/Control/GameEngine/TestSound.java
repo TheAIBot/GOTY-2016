4c2bb979768e237d0aa9acb424f159f24a2c7752
Lombre <jesper.birch3@gmail.com>
2016-01-08 09:33:26 +0100

    Made a somewhat working audiosystem

--- src/Control/GameEngine/TestSound.java
+++ src/Control/GameEngine/TestSound.java
@@ -3,7 +3,6 @@ package Control.GameEngine;
 public class TestSound {
 	
 	public static void main(String[] args) {
-		Sound meh = new Sound("res/bossdeath.wav");
-		meh.playSound();
+		Sound meh = new Sound("res/01 The Vampire From Nazareth.wav");
 	}
 }
