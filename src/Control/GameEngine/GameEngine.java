ae3a2ccac92cef0f9307e47b3374b41ac3fbfcfd
TheAIBot <andreasuranus@hotmail.com>
2016-01-10 01:47:18 +0100

    Redid the way rendering works

--- src/Control/GameEngine/GameEngine.java
+++ src/Control/GameEngine/GameEngine.java
@@ -1,37 +1,27 @@
 package Control.GameEngine;
 
-import java.awt.Rectangle;
-
 import Model.BoardChangedListener;
 import Model.GameBoard;
 import Model.GameBoardMode;
 import Model.GameSettings;
 import Model.GameState;
-import Model.GraphicsPanel;
-import Model.PlayerSettings;
 import Model.Tile;
-import View.Screen;
+import View.GraphicsPanel;
 import Model.Directions;
 
 public class GameEngine implements BoardChangedListener, KeyPressListener {
 	private final SaveFileManager<GameBoardMode> saver = new SaveFileManager<GameBoardMode>("saveFiles");
 	private final GraphicsManager graphics;
 	private final InputManager input = new InputManager();
+	private final GameSettings settings;
 	private GameBoardMode game;
-	private GameSettings settings;
 
-	public GameEngine(GameSettings settings, Screen screen, GraphicsPanel panel) {	
-		if (screen == null) {
-			throw new NullPointerException("Screen provided is null");
-		}//TODO add more null checks
-		this.settings = settings;
-		this.graphics = new GraphicsManager(screen, panel);		
-		initGame(settings);
-	}
-	
 	public GameEngine(GameSettings settings) {	
-		
-		graphics = null;
+		//if (screen == null) {
+			//throw new NullPointerException("Screen provided is null");
+		//}//TODO add more null checks
+		this.settings = settings;
+		this.graphics = new GraphicsManager();		
 		initGame(settings);
 	}
 	
@@ -120,9 +110,9 @@ public class GameEngine implements BoardChangedListener, KeyPressListener {
 	{
 		game.restart();
 	}
-	
-	public void windowResized(Rectangle newSize)
+
+	public GraphicsPanel getScreen()
 	{
-		graphics.windowResized(newSize, game.getTiles(), game.getRenderInfo());
+		return graphics.getGraphicsPanel();
 	}
 }
--- src/Menu/GOTYPlay.java
+++ src/Menu/GOTYPlay.java
@@ -1,52 +1,27 @@
 package Menu;
 
-import java.awt.Rectangle;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-
 import javax.swing.JPanel;
-
 import Control.GameEngine.GameEngine;
-import Model.CheatActivatedListener;
 import Model.DifficultyLevel;
 import Model.GameSettings;
-import Model.GraphicsPanel;
 import Model.SuperPage;
-import View.Screen;
 
 
 public class GOTYPlay extends SuperPage {
 	private GameEngine game;
-	private GraphicsPanel gPanel;
-	private Screen screen;
 	private GameSettings settings;
 
 	@Override
 	public JPanel createPage() {
-
-		gPanel = new GraphicsPanel(400,400);
-		screen = new Screen(gPanel.getGImage(), new Rectangle(gPanel.getImageBounds()));
-
-		page = gPanel;
-		return gPanel;
-		//return page;
+		settings.setGameSize(5);
+		settings.setDifficultyLevel(DifficultyLevel.NORMAL);
+		game = new GameEngine(settings);
+		page = game.getScreen();
+		return page;
 	}
 	
 	public void setGameSettings(GameSettings settings){
 		this.settings = settings;
-	}
-
-	@Override
-	public void startPage(SuperPage prevPage) {
-		super.startPage(prevPage);
-		settings.setGameSize(5);
-		settings.setDifficultyLevel(DifficultyLevel.NORMAL);
-		game = new GameEngine(settings, screen, gPanel);
-		gPanel.addComponentListener(new ComponentAdapter() {
-            public void componentResized(ComponentEvent e) {
-            	game.windowResized(gPanel.getBounds());
-            }
-        });
 	}
 
 	@Override
