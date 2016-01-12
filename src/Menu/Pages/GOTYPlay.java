1836fe06552c8bfb5e3436e317732894fda25468
TheAIBot <andreasuranus@hotmail.com>
2016-01-12 10:33:22 +0100

    Finished adding new image showing method

--- src/Menu/Pages/GOTYPlay.java
+++ src/Menu/Pages/GOTYPlay.java
@@ -18,7 +18,7 @@ public class GOTYPlay extends SuperPage {
 	
 	@Override
 	public JPanel createPage() {
-		settings.setGameSize(20);
+		settings.setGameSize(10);
 		settings.setDifficultyLevel(DifficultyLevel.HARD);
 		game = new GameEngine(settings);
 		page = game.getScreen();
