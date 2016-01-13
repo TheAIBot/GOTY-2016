780e417f3913059a0fb90cc505a0319cfad0bd75
TheAIBot <andreasuranus@hotmail.com>
2016-01-13 01:37:01 +0100

    made additional global values final in Tile

--- src/Game/Model/Board/Tile.java
+++ src/Game/Model/Board/Tile.java
@@ -23,11 +23,11 @@ import Game.View.Animation.ToAnimateListener;
 public class Tile implements java.io.Serializable, Displayable, Numreable, Colorfull, AnimationInfo {
 	private int scallingX = 1;
 	private int scallingY = 1;
-	private Point2D.Double[] corners;
-	private int number;
+	private final Point2D.Double[] corners;
+	private final int number;
 	transient Point2D.Double position;	
-	private Color color;
-	private Polygon colorPolygon;
+	private final Color color;
+	private final Polygon colorPolygon;
 	private static transient BufferedImage displayImage;
 	private Point2D.Double previousPosition;
 	private ToAnimateListener listener;
@@ -56,11 +56,11 @@ public class Tile implements java.io.Serializable, Displayable, Numreable, Color
 		this.position = position;
 		this.previousPosition = position;
 		this.color = color;	
-		corners = new Point2D.Double[]{new Point2D.Double(0, 0),
-				  			  new Point2D.Double(1, 0),
-				  			  new Point2D.Double(1, 1),
-				  			  new Point2D.Double(0, 1)};
-		colorPolygon = new Polygon();
+		this.corners = new Point2D.Double[]{new Point2D.Double(0, 0),
+				  			  				new Point2D.Double(1, 0),
+				  			  				new Point2D.Double(1, 1),
+				  			  				new Point2D.Double(0, 1)};
+		this.colorPolygon = new Polygon();
 		for (Point2D.Double corner : corners) {
 			colorPolygon.addPoint((int) corner.x, (int) corner.y);
 		}
