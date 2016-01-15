e5e8d5dbefdfab9fc7b2f1e7976d001b6265a8d3
TheAIBot <andreasuranus@hotmail.com>
2016-01-15 13:49:02 +0100

    fixed ResourceImages returning an empty list instead of null

--- src/Game/Model/Resources/ResourceImages.java
+++ src/Game/Model/Resources/ResourceImages.java
@@ -51,6 +51,7 @@ public class ResourceImages {
 		}
 		if (allImages.size() == 0) {
 			Log.writeln("Found no images at: " + path);
+			return null;
 		}
 		return allImages;
 	}
@@ -65,7 +66,7 @@ public class ResourceImages {
 	
 	private static BufferedImage loadImage(File imageFile) {
 		try {
-			return toCompatibleImage(ImageIO.read(imageFile));
+			return ImageIO.read(imageFile);
 		} catch (IOException e) {
 			Log.writeln("Something went wrong with the image loading process");
 			Log.writeError(e);
@@ -115,39 +116,5 @@ public class ResourceImages {
 	public static void releaseImageResource(BufferedImage image)
 	{
 		image.flush();
-	}
-	
-	/**
-	 * http://stackoverflow.com/questions/196890/java2d-performance-issues
-	 * @param image
-	 * @return
-	 */
-	private static BufferedImage toCompatibleImage(BufferedImage image)
-	{
-		// obtain the current system graphical settings
-		GraphicsConfiguration gfx_config = GraphicsEnvironment.
-			getLocalGraphicsEnvironment().getDefaultScreenDevice().
-			getDefaultConfiguration();
-
-		/*
-		 * if image is already compatible and optimized for current system 
-		 * settings, simply return it
-		 */
-		if (image.getColorModel().equals(gfx_config.getColorModel()))
-			return image;
-
-		// image is not optimized, so create a new image that is
-		BufferedImage new_image = gfx_config.createCompatibleImage(
-				image.getWidth(), image.getHeight(), gfx_config.getColorModel().getTransparency());
-
-		// get the graphics context of the new image to draw the old image on
-		Graphics2D g2d = (Graphics2D) new_image.getGraphics();
-
-		// actually draw the image and dispose of context no longer needed
-		g2d.drawImage(image, 0, 0, null);
-		g2d.dispose();
-
-		// return the new optimized image
-		return new_image; 
 	}
 }
