81026ab467020ef035274a836507bbff68ca4a9c
TheAIBot <andreasuranus@hotmail.com>
2016-01-17 17:01:15 +0100

    fixed bug where console updated with time

--- src/Game/Model/Board/SinglePlayerBoard.java
+++ src/Game/Model/Board/SinglePlayerBoard.java
@@ -388,7 +388,7 @@ public class SinglePlayerBoard implements GameBoardMode, java.io.Serializable, T
 
 	@Override
 	public void unpause() {
-		if (isRunning) {
+		if (isRunning && settings.hasGUI()) {
 			scoreManager.startClock();
 		}
 	}
